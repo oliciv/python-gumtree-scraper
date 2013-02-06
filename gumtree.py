e4afdb5195c3b5852c5dace6223a912a628b2978
Oli Allen <oli@oliallen.com>
2013-02-06 12:36:01 +0000

    Collect basic information from listing pages

--- gumtree.py
+++ gumtree.py
@@ -24,7 +24,7 @@ class SearchListing:
         self.category = category
         self.query = query    
         self.location = location
-        self.doSearch()
+        self.listing_results = self.doSearch()
 
     def __str__(self):
         return "Search listing"
@@ -34,14 +34,33 @@ class SearchListing:
         Performs the search against gumtree
         """
 
-        request_headers = {'User-agent': 'Mozilla/5.0',}        
+        request_headers = {'User-agent': USER_AGENT,}
 
         request = requests.get("http://www.gumtree.com/motorbikes-scooters", headers=request_headers)
 
         if request.status_code == 200:
             # Got a valid response
+
+            listing_results = []
+
             souped = BeautifulSoup(request.text, "html5lib")
-            print souped.find_all("ul", class_="ad-listings")
+            for listings_wrapper in souped.find_all("ul", class_="ad-listings"):
+                for listing in listings_wrapper.find_all("li", class_="offer-sale"):
+                    title = listing.find("a", class_="description").get("title")
+                    item_instance = GTItem(title=title)
+                    item_instance.price = listing.find("span", class_="price").string
+                    item_instance.summary = listing.find("div", class_="ad-description").find("span").string
+                    item_instance.location =  listing.find("span", class_="location").string
+                    item_instance.thumbnail = listing.find("img", class_="thumbnail").get("src")
+                    listing_results.append(item_instance)
+            return listing_results
         else:
             # TODO: Add error handling
             return []
+
+class GTItem:
+    def __init__(self, title, summary="", description="", thumbnail="", price="", location="", adref="", url="", contact_name="", contact_number=""):
+        self.title = title
+
+    def __str__(self):
+        return self.title
