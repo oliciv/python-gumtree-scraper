38dbf160e149710675324239172fa88225b9cab6
Oli Allen <oli@oliallen.com>
2013-02-06 12:08:41 +0000

    Changed parser to html5lib

--- gumtree.py
+++ gumtree.py
@@ -5,11 +5,16 @@ Gumtree scraper written in Python
 
 Copyright 2013 Oli Allen <oli@oliallen.com>
 
-
 THIS SOFTWARE IS SUPPLIED WITHOUT WARRANTY OF ANY KIND, AND MAY BE
 COPIED, MODIFIED OR DISTRIBUTED IN ANY WAY, AS LONG AS THIS NOTICE
 AND ACKNOWLEDGEMENT OF AUTHORSHIP REMAIN.
 """
+
+USER_AGENT = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)"
+
+import requests
+from bs4 import BeautifulSoup
+
 class SearchListing:
     """
     A gumtree search result set containing GTItem objects
@@ -24,8 +29,19 @@ class SearchListing:
     def __str__(self):
         return "Search listing"
 
-    def doSearch():
+    def doSearch(self):
         """
         Performs the search against gumtree
         """
-        
+
+        request_headers = {'User-agent': 'Mozilla/5.0',}        
+
+        request = requests.get("http://www.gumtree.com/motorbikes-scooters", headers=request_headers)
+
+        if request.status_code == 200:
+            # Got a valid response
+            souped = BeautifulSoup(request.text, "html5lib")
+            print souped.find_all("ul", class_="ad-listings")
+        else:
+            # TODO: Add error handling
+            return []
